1d75ea00eb53623352fe0f857ac9ea62401a4c23
Beto Quezada <Beetoo1313@gmail.com>
2020-05-29 20:16:47 -0600

    Envío de datos a recyclerView listo, falta hacer ruta del fragmentDetalles

--- routes/fragmentHistorial.js
+++ routes/fragmentHistorial.js
@@ -9,23 +9,27 @@ let num_documentos_enviar = 0;
 
 router.get("/historial", (req, res) => {
   Conjunto.countDocuments({}, (err, numeroDocumentosRecibido) => {
-    console.log("Numero de documentos: " + numeroDocumentosRecibido);
+    //console.log("Numero de documentos: " + numeroDocumentosRecibido);
     num_documentos_enviar = numeroDocumentosRecibido;
   });
 
-  Conjunto.findOne({}, (err, concepts) => {
-    if (err) {
-      return res
-        .status(500)
-        .send({ message: `Problem with the searching request ${err}` });
-    }
+  Conjunto.find(
+    {},
+    { _id: 0, "imgConjunto.img64conjunto": 1, savedAt: 1 },
+    (err, concepts) => {
+      if (err) {
+        return res
+          .status(500)
+          .send({ message: `Problem with the searching request ${err}` });
+      }
 
-    res.status(200).json({
-      message: "Request complete",
-      numeroDocs: num_documentos_enviar,
-      conjuntos: concepts,
-    });
-  });
+      res.status(200).json({
+        message: "Request complete",
+        numeroDocs: num_documentos_enviar,
+        conjuntos: concepts,
+      });
+    }
+  );
 });
 
 module.exports = router;
---
+++ routes/fragmentHistorialDetalles.js
@@ -0,0 +1,7 @@
+const { Router } = require("express");
+const router = Router();
+
+//Modelo
+const Conjunto = require("../src/models/ConjuntoSchema");
+
+router.get("/detallesHistorial", (req, res) => {});
