1529ba03d4cbf6f97a3c7442bb4e1c2d42bf1620
Beto Quezada <Beetoo1313@gmail.com>
2020-05-31 02:38:32 -0600

    ruta de detalles del historial funcionando

--- routes/fragmentHistorialDetalles.js
+++ routes/fragmentHistorialDetalles.js
@@ -4,4 +4,30 @@ const router = Router();
 //Modelo
 const Conjunto = require("../src/models/ConjuntoSchema");
 
-router.get("/detallesHistorial", (req, res) => {});
+//Variables
+let fecha = "";
+
+router.get("/detallesHistorial", (req, res) => {
+  console.log("headers ", req.get("fecha"));
+  let body = req.get("fecha");
+  fecha = body;
+  console.log(fecha);
+
+  Conjunto.find(
+    { savedAt: fecha },
+    {
+      _id: 0,
+      "imgTop.img64top": 1,
+      "imgBottom.img64bottom": 1,
+      "imgMisc.img64misc": 1,
+      "imgShoes.img64shoes": 1,
+      "imgConjunto.img64conjunto": 1,
+    },
+    (err, concepts) => {
+      //console.log(concepts);
+      res.send({ message: "request succesfull", concepts: concepts });
+    }
+  );
+});
+
+module.exports = router;
--- src/index.js
+++ src/index.js
@@ -1,25 +1,26 @@
-const express = require('express')
-const app = express()
-const bodyParser =require('body-parser')
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
 // const multer = require('multer')
 // const fs = require('fs')
 
 //Settings
-app.set('port', process.env.Port || 3000);
-app.use(bodyParser.json({limit: '50mb', extended: true}))
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}))
+app.set("port", process.env.Port || 3000);
+app.use(bodyParser.json({ limit: "50mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 
 //Middlewares
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 //Routes
-app.get('/', (req, res) => {
-    res.send("Simón ya anda jalando")
+app.get("/", (req, res) => {
+  res.send("Simón ya anda jalando");
 });
 
-app.use(require('../routes/tipoRopa'));
-app.use(require('../routes/conjunto'));
-app.use(require('../routes/fragmentHistorial'));
+app.use(require("../routes/tipoRopa"));
+app.use(require("../routes/conjunto"));
+app.use(require("../routes/fragmentHistorial"));
+app.use(require("../routes/fragmentHistorialDetalles"));
 
 module.exports = app;
